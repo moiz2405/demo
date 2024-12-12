f986e2b198b9d0aa2737c4d502e4a25c0fb4c400
Moiz2405 <km856307@gmail.com>
2024-12-12 21:28:46 +0530

    added navabr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 
 export default function RootLayout({
   children,
@@ -7,6 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body >
+        <Navbar />
         {children}
       </body>
     </html>
