78bab2e38f1f80e289d792d1802c9dfffb40a226
Moiz2405 <km856307@gmail.com>
2024-12-12 21:30:10 +0530

    added user page

---
+++ app/user/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "@/components/Navbar";
+import Footer from '@/components/Footer';
+function page() {
+  return (
+    <>
+    <Footer/>
+    </>
+  )
+}
+
+export default page
